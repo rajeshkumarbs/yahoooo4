22e2595d23fe7c26f4590f1bcba8ad56858a2693
rajeshkumarbs <59757677+rajeshkumarbs@users.noreply.github.com>
2020-01-11 13:27:00 +0530

    Update demo2.java

--- src/qsp/demo2.java
+++ src/qsp/demo2.java
@@ -5,6 +5,8 @@ public class demo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("uploading modified script");
+		System.out.println("new uploading modified script");
+		System.out.println("new uploading modified script");
 	}
 
 }
